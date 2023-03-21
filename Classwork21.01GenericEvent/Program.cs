9fd5398f3ec0a7516ceec88ecb7d61d2da40cd94
mariam <mariam@MYPROBOOK>
2023-03-21 21:18:10 +0400

    Add project files.

---
+++ Classwork21.01GenericEvent/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Classwork21._01GenericEvent
+{
+    internal class Program
+    {
+        public class GenericClass<T> { }
+        public class GenericClass1<T> 
+        {
+            public void Meth(T t)
+            {
+                T a = t ;
+                Console.WriteLine(a);
+
+            }   
+        }
+
+        static void Main(string[] args)
+        {
+            GenericClass1<string> a = new GenericClass1<string>();
+            a.Meth("hey");
+        }
+    }
+}
